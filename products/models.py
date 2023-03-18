01bb7cd0263e7ed872b83f6c4df9fbe9c86946ac
AminZamani <aminzamani.work@gmail.com>
2023-03-18 15:03:15 +0330

    product class model was created and it's fields were specified

--- products/models.py
+++ products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField(blank=True, max_length=500)
+
+    # an introduction of the product, Support HTML formatting.
+    introduction = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updates_at = models.DateTimeField(auto_now=True, )
+
+    # Model Meta is basically used to change the behavior of your model fields like changing order options
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.name
