37c9050676a013d1473c7897e5f84cbbeca8f9fc
Etti <ettifeyzi@gmail.com>
2023-03-24 21:45:38 +0430

    three views class created.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
-# Create your views here.
+from . import serializers
+
+User = get_user_model()
+
+
+class RegisterView(GenericAPIView):
+    """
+    An endpoint for the client to create a new User.
+    """
+
+    permission_classes = (AllowAny,)
+    serializer_class = serializers.UserRegisterationSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        if user:
+            token = Token.objects.create(user=user)
+            data = serializer.data
+            data['token'] = token.key
+            return Response(data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+     
+class LoginView(GenericAPIView):
+    """
+    An endpoint to authenticate existing users using their email and password.
+    """
+    permission_classes = (AllowAny,)
+    serializer_class = serializers.UserLoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        serializer = serializers.CustomUserSerializer(user)
+        token = Token.objects.get(user=user)
+        data = serializer.data
+        data['token'] = token.key
+        return Response(data, status=status.HTTP_200_OK)
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def User_logout_user(request):
+
+    request.user.auth_token.delete()
+
+    # logout(request)
+    return Response('logout')
