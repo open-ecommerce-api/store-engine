fe76029188d48a33a753f71a59586e9941bb23e5
Etti <ettifeyzi@gmail.com>
2023-03-26 10:17:31 +0430

    three endpoints were added.

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import RegisterView, LoginView, User_logout_user
+from .views import RegisterView, LoginView, User_logout_user, adminLogin, ChangePasswordView, ResetPassword
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register' ),
     path('login/', LoginView.as_view(), name='login' ),
     path('logout/', User_logout_user, name='logout'),
+
+    path('adminlogin/', adminLogin, name='adminlogin' ),
+
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('reset-password/', ResetPassword.as_view(),name='reset-password'), 
 
 ]
