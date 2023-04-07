c563e6d0f927f587e7734a80aa065052c8e283da
Etti <ettifeyzi@gmail.com>
2023-04-07 15:30:48 +0430

    added an endpoint to verify user

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegisterView, LoginView, User_logout_user, adminLogin, ChangePasswordView, ResetPassword
+from .views import RegisterView, LoginView, User_logout_user, adminLogin, ChangePasswordView, ResetPassword, VerifyEmail
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register' ),
+    path('verify_email/', VerifyEmail.as_view(), name='verify_email'),
     path('login/', LoginView.as_view(), name='login' ),
     path('logout/', User_logout_user, name='logout'),
 
