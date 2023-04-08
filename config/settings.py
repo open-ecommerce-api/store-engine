f6ac903a3ba7cee81c5fb418e24651ad22d3f7ff
AminZamani <aminzamani.work@gmail.com>
2023-04-08 17:22:29 +0330

    Add `User` model
    - admin code deleted, not necessary.
    - form file deleted, not usable for now.

--- config/settings.py
+++ config/settings.py
@@ -167,4 +167,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'users.CustomUser'
+AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -2,6 +2,5 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class CustomUser(AbstractUser):
-    age = models.PositiveIntegerField(null=True, blank=True)
-    phone = models.CharField(max_length=11, unique=True, default='0')
+class User(AbstractUser):
+    pass
