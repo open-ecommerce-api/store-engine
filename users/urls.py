e86c1a8f83883250ff033bb76d60dabac42a8ca4
AminZamani <aminzamani.work@gmail.com>
2023-04-10 18:23:53 +0330

    Update `urls.py`, two new routes added for signup a user.

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('signin/', views.SigninView.as_view(), name='signin'),
+    path('signup/', views.SignupView.as_view(), name='signup'),
+    path('signup/confirm/<str:token>', views.ConfirmSignupView.as_view(), name='confirm_signup'),
 ]
