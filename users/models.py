0ae1b38d5107cd8d524de165643aecd66f25d093
AminZamani <aminzamani.work@gmail.com>
2023-04-10 18:28:19 +0330

    Fix `username`, now email and username have the same value
    but in nex release, we must write an ability to allow users to change their username as what they want.

--- users/models.py
+++ users/models.py
@@ -61,9 +61,14 @@ class UserManager(BaseUserManager):
 
 class User(AbstractUser):
     email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, blank=False, null=False)
     USERNAME_FIELD = 'email'
 
     # fix error [users.User: (auth.E002)], so you should remove 'email' from the 'REQUIRED_FIELDS', like this.
     REQUIRED_FIELDS = []
-    
+
     objects = UserManager()
+
+    def save(self, *args, **kwargs):
+        self.username = self.email
+        super().save(*args, **kwargs)
