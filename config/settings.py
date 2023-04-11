e8d471371d381bd75faae67f6c98f4f1e85ed198
AminZamani <aminzamani.work@gmail.com>
2023-04-11 03:49:07 +0330

    Added login button to DRF navbar

--- config/settings.py
+++ config/settings.py
@@ -54,7 +54,11 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.AllowAny",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
 
 SPECTACULAR_SETTINGS = {
