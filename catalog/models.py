efee4c1e4fabbe5b52d97727fef04e08cac3e66b
AminZamani <aminzamani.work@gmail.com>
2023-04-14 04:30:45 +0330

    Added `product models`
    this is a base model and it may change

--- catalog/models.py
+++ catalog/models.py
@@ -2,14 +2,42 @@ from django.db import models
 
 
 class ProductType(models.Model):
-    """
-    `ProductType` in ecommerce refers to the category or type of product being sold.
-    It is a way of organizing and classifying products based on their characteristics and intended use.
-    For example, in a fashion ecommerce store, the product types could be clothing, shoes, and accessories.
-    In a grocery ecommerce store, the product types could be fresh produce, canned goods, and snacks.
-    Product types are important in ecommerce because they help customers quickly and easily find the products they are
-    looking for, and they also help ecommerce businesses manage their inventory and product listings.
-    By using product types, businesses can more effectively track their sales and make data-driven decisions about
-    which types of products to stock and promote.
-    """
     name = models.CharField(max_length=255)
+
+
+class Product(models.Model):
+    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    is_published = models.BooleanField(default=False)
+
+
+class Variant(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+
+class VariantItem(models.Model):
+    variant = models.ForeignKey(Variant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+
+class Category(models.Model):
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=255)
+
+
+class ProductMedia(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    file = models.ImageField(upload_to='product_media')
+
+
+class ProductVariant(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variant = models.ForeignKey(Variant, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    stock = models.IntegerField(default=0)
+
+    # sku = models.CharField(max_length=255, unique=True)
+    # name = models.CharField(max_length=255)
+    # track_inventory = models.BooleanField(default=True)
