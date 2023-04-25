b100e3415acd3518ab22bd818c875e8d7a8b38d3
MohammadAmin Forouzan <maf081378@gmail.com>
2023-04-25 05:46:23 +0330

    Update settings.py django_rest_framework
    remove basic authentication from default authentication classes

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,6 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
