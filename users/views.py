b5df6878e0b4b6c9a92bd2d07799eeaf6f3d70bf
MohammadAmin Forouzan <maf081378@gmail.com>
2023-06-11 10:20:31 +0330

    Remove session authentication

--- users/views.py
+++ users/views.py
@@ -37,11 +37,9 @@ class SigninView(GenericAPIView):
         """
         if serializer.is_valid():
             user = serializer.validated_data['user']
+
             # this line of code will resolve Token error for superuser:
             token, _ = Token.objects.get_or_create(user=user)
-
-            # update the `last_login` field in the user table
-            login(request, user)
 
             return Response({'token': token.key}, status=status.HTTP_200_OK)
         else:
@@ -114,11 +112,8 @@ class LogoutView(APIView):
         """
         try:
             Token.objects.filter(user=request.user).delete()
-
-            # Logout from sessions
-            logout(request)
-
             return Response({"message": "You have been logged out."}, status=status.HTTP_200_OK)
+
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
