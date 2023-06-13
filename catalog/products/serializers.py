138919e6128d56d835ec113087f0701d71fe8cd2
AminZamani <aminzamani.work@gmail.com>
2023-06-13 19:49:54 +0330

    update, product
    restructure product

--- catalog/products/serializers.py
+++ catalog/products/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import ProductOption, ProductOptionValue, Product
+from catalog.models import ProductOption, ProductOptionValue, Product
 
 
 class ProductOptionSerializer(serializers.ModelSerializer):
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import ProductViewSet
+from catalog.products.views import ProductViewSet
 from catalog.attributes.views import AttributeView, AttributeValueView
 
 router = routers.DefaultRouter()
-router.register(r'products', ProductViewSet)
+# router.register(r'products', ProductViewSet)
 router.register(r'attributes', AttributeView)
 router.register(r'attribute-values', AttributeValueView)
 
