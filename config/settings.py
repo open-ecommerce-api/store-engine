2d2c10c2fd0183673e42653d879539c3eb0dfcc8
AminZamani <aminzamani.work@gmail.com>
2023-06-18 16:20:33 +0330

    update, settings

--- config/settings.py
+++ config/settings.py
@@ -174,5 +174,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
-DEFAULT_FROM_EMAIL = 'noreply@pysell.ir'
+DEFAULT_FROM_EMAIL = 'noreply@example.com'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
