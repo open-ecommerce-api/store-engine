81a987ae4412c4e717900358407cf1a342804874
MohammadAmin Forouzan <maf081378@gmail.com>
2023-07-07 22:46:58 +0330

    Add totp to models
    - include a method to save totp
    - include a method to verify totp

--- users/models.py
+++ users/models.py
@@ -1,11 +1,21 @@
+from datetime import timedelta
+
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.timezone import now
+
 from .managers import UserManager
+from .utils import generate_otp
 
 
 class User(AbstractUser):
     email = models.EmailField(max_length=255, unique=True)
-    username = models.CharField(max_length=255, blank=False, null=False)
+    username = models.CharField(max_length=255, blank=False, null=False, unique=True)
+
+    totp = models.CharField(max_length=12, null=True)
+    totp_valid_until = models.DateTimeField(default=now)
+
     USERNAME_FIELD = 'email'
 
     # fix error [users.User: (auth.E002)], so you should remove 'email' from the 'REQUIRED_FIELDS', like this.
@@ -16,3 +26,22 @@ class User(AbstractUser):
     def save(self, *args, **kwargs):
         self.username = self.email
         super().save(*args, **kwargs)
+
+    def save_totp(self, length=6):
+        """
+        saves a (default 6-digit) timed based otp for user
+        """
+        self.totp = generate_otp(length)
+        self.totp_valid_until = now() + timedelta(seconds=settings.OTP_EXPIRATION)
+        self.save()
+
+    def validate_totp(self, otp: str) -> bool:
+        if (
+                (self.totp is not None)
+                and (self.totp == otp)
+                and (now() < self.totp_valid_until)
+        ):
+            self.totp = None
+            self.totp_valid_until = now()
+            return True
+        return False
