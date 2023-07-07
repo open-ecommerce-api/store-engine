c631d14bc3e1de95d5ff91c710d622f24a27c0ba
MohammadAmin Forouzan <maf081378@gmail.com>
2023-07-07 22:51:03 +0330

    Add change email confirm path & Remove the token from signup confirm

--- users/urls.py
+++ users/urls.py
@@ -4,10 +4,11 @@ from . import views
 urlpatterns = [
     path('signin/', views.SigninView.as_view(), name='signin'),
     path('signup/', views.SignupView.as_view(), name='signup'),
-    path('signup/confirm/<str:token>', views.ConfirmSignupView.as_view(), name='confirm_signup'),
+    path('signup/confirm/', views.ConfirmSignupView.as_view(), name='confirm_signup'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/<str:token>', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('change_password/', views.ChangePasswordView.as_view(), name='change_password'),
     path('change_email/', views.ChangeEmailView.as_view(), name='change_email'),
+    path('change_email/confirm/', views.ChangeEmailConfirmView.as_view(), name='change_email_confirm'),
 ]
