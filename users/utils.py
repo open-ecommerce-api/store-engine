97819a9eae9ef3b711c9fba279d44a7a485897cf
MohammadAmin Forouzan <maf081378@gmail.com>
2023-07-10 09:31:10 +0330

    Add get_user function .
    this function extracts the user from urlsafe encoded pk

--- users/utils.py
+++ users/utils.py
@@ -1,6 +1,9 @@
 from string import digits
 
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.utils.crypto import get_random_string
+from django.utils.http import urlsafe_base64_decode
 
 
 def generate_otp(length) -> str:
@@ -8,3 +11,22 @@ def generate_otp(length) -> str:
     returns an all-numeric OTP code with custom length
     """
     return get_random_string(length=length, allowed_chars=digits)
+
+
+def get_user(uidb64):
+    """
+    Retrieves the user object from a stored pk in urlsafe encoded base64 value
+    """
+    try:
+        # urlsafe_base64_decode() decodes to bytestring
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = get_user_model()._default_manager.get(pk=uid)
+    except (
+            TypeError,
+            ValueError,
+            OverflowError,
+            get_user_model().DoesNotExist,
+            ValidationError,
+    ):
+        user = None
+    return user
