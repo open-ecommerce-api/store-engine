1cf9b882024fa66e8ea295ba18d7c0892b55d4da
AminZamani <aminzamani.work@gmail.com>
2023-07-11 22:37:11 +0330

    update, models

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,34 +1,47 @@
 from django.db import models
 
 
-class ProductType(models.Model):
-    """
-    A categorization for the product used for filtering and searching products.
-    """
-    name = models.CharField(max_length=255)
-
-
 class Product(models.Model):
-    # product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE)
+    """
+    The Product resource lets you update and create products in a merchant's store.
+    You can use product variants with the Product resource to create or update different versions of the same product.
+    You can also add or update product images.
+    """
     name = models.CharField(max_length=255)
+
+    # A description of the product. Supports HTML formatting.
     description = models.TextField()
-    is_published = models.BooleanField(default=False)
 
-    """
-    save selected options with it's items
-    """
+    # the status of the product. valid values: "active, archived, draft"
+    # `active`: The product is ready to sell and is available to customers on the online store, sales channels,
+    # and apps. by default, existing products are set to active.
+    # `archived`: The product is no longer being sold and isn't available to customers on sales channels and apps.
+    # `draft`: The product isn't ready to sell and is unavailable to customers on sales channels and apps.
+    status = models.CharField(default=False)
 
-    # solution
-    selected_options = models.JSONField(blank=True, null=True)
-    #
+    # the date and time when the product was created.
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # The date and time when the product was last modified.
+    # A product's updated_at value can change for different reasons.
+    # For example, if an order is placed for a product that has inventory tracking set up,
+    # then the inventory adjustment is counted as an update.
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # The date and time when the product was published.
+    published_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 
 class ProductOption(models.Model):
     """
-    To enhance search ability, include descriptive information like "Material" or "Brand" for customers to use when
+    To enhance search ability, include descriptive information like "Color" or "Size" for customers to use when
     searching on your store.
     Each product can have a maximum of 3 options, such as Size, Color, and Material.
     """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
@@ -46,36 +59,57 @@ class ProductOptionItem(models.Model):
         return self.item
 
 
-class Category(models.Model):
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=255)
-
-
-class ProductMedia(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    file = models.ImageField(upload_to='product_media')
-
-
 class ProductVariant(models.Model):
     """
     Product variants are created by combining different option items.
     """
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    # variant = models.ForeignKey(ProductOption, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=12, decimal_places=2)
     stock = models.IntegerField(default=0)
-    sku = models.CharField(max_length=255, unique=True)
-    # track_inventory = models.BooleanField(default=True)
 
+    option_item_1 = models.ForeignKey(
+        ProductOptionItem, related_name='option_item_1', on_delete=models.CASCADE, null=True, blank=True)
+
+    option_item_2 = models.ForeignKey(
+        ProductOptionItem, related_name='option_item_2', on_delete=models.CASCADE, null=True, blank=True)
+
+    option_item_3 = models.ForeignKey(
+        ProductOptionItem, related_name='option_item_3', on_delete=models.CASCADE, null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+# class ProductMedia(models.Model):
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     file = models.ImageField(upload_to='product_media')
+
+class ProductManager:
     """
-    save variant combination
+    [] create a variable product
+    [] generate option base on saved attributes
+    [] generate variants by options combination, max is 3 options
+    [] if admin wants to edit the options, so how to manage variants if it is in the order list?
+    []
     """
 
-    # solution 1
-    # save each option-item-id
-    # ---------------------------------------------------------------------------------------------
-    # option1 = models.ForeignKey(ProductOptionItem, on_delete=models.CASCADE, null=True, blank=True)
-    # option2 = models.ForeignKey(ProductOptionItem, on_delete=models.CASCADE, null=True, blank=True)
-    # option3 = models.ForeignKey(ProductOptionItem, on_delete=models.CASCADE, null=True, blank=True)
-    # ---------------------------------------------------------------------------------------------
+    def create_product(self, name):
+        product = Product.objects.create(name=name)
+
+        # get attributes
+        # generate options
+        # save options
+        # generate variants
+        # save variants
+
+    def retrieve_product(self):
+        ...
+
+    def delete_product(self):
+        ...
+
+    def update_product(self):
+        ...
+
+    def create_options(self, product, attributes):
+        ...
