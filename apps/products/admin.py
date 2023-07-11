c68324ee289cdd0c1e2b1e363c24eab01da485b6
AminZamani <aminzamani.work@gmail.com>
2023-07-11 22:39:00 +0330

    update, product
    some models added to django admin for test the models

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from apps.products.models import Product, ProductOption, ProductOptionItem, ProductVariant
+from apps.attributes.models import Attribute, AttributeItem
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class ProductOptionItemInline(admin.TabularInline):
+    model = ProductOptionItem
+    extra = 0
+
+
+@admin.register(ProductOption)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    inlines = [ProductOptionItemInline]
+
+
+@admin.register(ProductOptionItem)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('item',)
+
+# @admin.register(ProductVariant)
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ('name',)
