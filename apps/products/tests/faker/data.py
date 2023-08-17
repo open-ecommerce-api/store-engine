3a1f14ab39aa46397bffa50cc199faa1dbc1a95c
AminZamani <aminzamani.work@gmail.com>
2023-08-17 11:30:55 +0330

    update, product test
    - added some tests
    - added some todo list
    - added fake data generator, I will use this data for Demo and Test purpose
    
    not completed, and needs more job, I committed it just for save the progress

--- apps/products/tests/faker/data.py
+++ apps/products/tests/faker/data.py
@@ -8,10 +8,73 @@ class FakeProduct:
     Populates the database with initial data
     """
 
-    # product_name = fa
+    fake = Faker()
+
+    product_name = ''
+    product_description = ''
+    product_status = ''
+    product_options = []
+
+    options = ['color', 'size', 'material']
+    option_color_items = ['red', 'green', 'black', 'blue', 'yellow']
+    option_size_items = ['S', 'M', 'L', 'XL', 'XXL']
+    option_material_items = ['Cotton', 'Nylon', 'Plastic', 'Wool', 'Leather']
+
+    def __init__(self):
+        self.product_name = self.generate_name()
+        self.product_description = self.generate_description()
+        self.product_options = self.generate_options()
 
     def fill_products(self):
+        self.fake.add_provider(lorem)
         self.populate_products()
+
+    def populate_single_product(self):
+        return Product.objects.create_product(
+            {
+                "product_name": self.product_name,
+                "description": self.product_description,
+                "status": "draft",
+                "options": self.product_options
+            }
+        )
+
+    def generate_inputs_for_single_product(self):
+        return {
+            "product_name": FakeProduct.product_name,
+            "description": FakeProduct.product_description,
+            "status": FakeProduct,
+            "options": FakeProduct
+        }
+
+    def generate_name(self):
+        return self.fake.text(max_nb_chars=50)
+
+    def generate_description(self):
+        return self.fake.paragraph(nb_sentences=50)
+
+    def generate_status(self):
+        return "draft"
+
+    def generate_options(self):
+        return [
+            {
+                "option_name": "color",
+                "items": self.option_color_items[:2]
+            },
+            {
+                "option_name": "size",
+                "items": self.option_size_items[:2]
+            },
+            {
+                "option_name": "material",
+                "items": self.option_material_items[:2]
+            },
+            {
+                "option_name": "color",
+                "items": self.option_color_items[:3]
+            }
+        ]
 
     def populate_products(self):
         fake = Faker()
--- apps/products/tests/test_create_product.py
+++ apps/products/tests/test_create_product.py
@@ -1,14 +1,26 @@
 import random
-
 from rest_framework import status
-from faker import Faker
-from faker.providers import lorem
 from rest_framework.utils import json
 
 from apps.attributes.tests.base_test_case import BaseTestCase
-from apps.products.tests.faker.data import FakeProduct as _Data
+from apps.products.tests.faker.data import FakeProduct
+from apps.products.models import Product
 
 
+# todo[] test create_product permissions
+# todo[] test create_product without required fields
+# todo[] test create_product with empty payload
+# todo[] test create_product with blank fields
+# todo[] test create_product with invalid payload
+# todo[] test create_product blank required fields
+# todo[] test create_product if status value is anything other than ('active', 'archived', 'draft')
+
+# todo[] test create_product with empty options
+# todo[] test create_product with invalid option data
+# todo[] test create_product 0 or 3 option
+# todo[] test create_product max 3 options
+# todo[] Test create_product with duplicate option-names (should be unique for a product)
+# todo[] Test create_product with duplicate item-names (should be unique for a product-option)
 class ProductViewTest(BaseTestCase):
     """
     Test access permissions for CRUD methods
@@ -17,14 +29,6 @@ class ProductViewTest(BaseTestCase):
 
     # init data
     product_endpoint = "/admin/products/"
-
-    fake = Faker()
-
-    options = ['color', 'size', 'material']
-
-    option_color_items = ['red', 'green', 'black', 'blue', 'yellow']
-    option_size_items = ['S', 'M', 'L', 'XL', 'XXL']
-    option_material_items = ['Cotton', 'Nylon', 'Plastic', 'Wool', 'Leather']
 
     @classmethod
     def setUpTestData(cls):
@@ -35,7 +39,8 @@ class ProductViewTest(BaseTestCase):
         super().setUpTestData()
 
         # fill database
-        cls.fake.add_provider(lorem)
+        cls.fake_product = FakeProduct()
+        # FakeProduct().populate_single_product()
 
     def test_access_permission(self):
         # todo[] test access permissions
@@ -45,14 +50,83 @@ class ProductViewTest(BaseTestCase):
 
     def test_create_product(self):
         """
+        Test create a product by the all available inputs (as valid data)
+        """
+
+        self.set_admin_authorization()
+
+        payload = json.dumps({
+            "product_name": self.fake_product.generate_name(),
+            "description": self.fake_product.generate_description(),
+            "status": "draft",
+            "options": self.fake_product.generate_options()
+        })
+
+        response = self.client.post(self.product_endpoint, data=payload, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        product_id = response.data['product_id']
+        product = Product.objects.get(pk=product_id)
+        options = Product.objects.retrieve_options(product_id)
+
+        # To see the entire diff and identify the specific differences between the two JSON objects:
+        # self.maxDiff = None
+
+        self.assertEqual(response.json(), {
+            'product_id': product_id,
+            'product_name': product.product_name,
+            'description': product.description,
+            'status': product.status,
+            'created_at': product.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'options': options
+        })
+
+    def test_create_product_with_empty_payload(self):
+        ...
+
+    def test_create_product_with_blank_fields(self):
+        ...
+
+    def test_create_product_with_invalid_payload(self):
+        ...
+
+    def test_create_product_without_required_fields(self):
+        ...
+
+    def test_create_product_just_required_field(self):
+        ...
+
+    def test_create_product_with_duplicate_option_names(self):
+        ...
+
+    def test_create_product_with_duplicate_item_names(self):
+        ...
+
+    def test_save_with_valid_status(self):
+        ...
+
+    def test_save_with_invalid_status(self):
+        ...
+
+    def test_retrieve_product_details(self):
+        """
+        Test retrieve a single product details
+        """
+
+        self.set_admin_authorization()
+
+        request = self.client.get(self.product_endpoint)
+
+    def test_create_product_by_random_options(self):
+        """
         Test create product by admin permission
         Test the all input fields name as a jason payload and also in the response body
         """
 
         self.set_admin_authorization()
 
-        product_name = self.fake.text(max_nb_chars=50)
-        description = self.fake.paragraph(nb_sentences=50)
+        product_name = self.fake_product.generate_name(max_nb_chars=50)
+        description = self.fake_product.generate_description(nb_sentences=50)
         product_status = "draft"
         options = self.generate_random_options()
 
@@ -75,17 +149,6 @@ class ProductViewTest(BaseTestCase):
         # })
 
     def test_create_product_invalid_payload(self):
-        """
-        todo[*] test with empty payload
-        todo[] test with blank fields
-        todo[] test without required fields
-        todo[] test if status value is anything other than ('active', 'archived', 'draft')
-        todo[] test same 'option names'
-        todo[] test same 'item names'
-        todo[] test 0 or 3 option
-        todo[] test max 3 options
-        todo[] test
-        """
 
         self.set_admin_authorization()
 
