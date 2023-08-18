cda1b4d8bbbff01f6fb698be3e80799430c0dc3d
AminZamani <aminzamani.work@gmail.com>
2023-08-18 12:32:01 +0330

    update, product faker
    new methods added

--- apps/products/tests/faker/data.py
+++ apps/products/tests/faker/data.py
@@ -1,3 +1,5 @@
+import random
+
 from faker import Faker
 from faker.providers import lorem
 from apps.products.models import Product
@@ -15,15 +17,16 @@ class FakeProduct:
     product_status = ''
     product_options = []
 
-    options = ['color', 'size', 'material']
+    options = ['color', 'size', 'material', 'Style']
     option_color_items = ['red', 'green', 'black', 'blue', 'yellow']
     option_size_items = ['S', 'M', 'L', 'XL', 'XXL']
     option_material_items = ['Cotton', 'Nylon', 'Plastic', 'Wool', 'Leather']
+    option_style_items = ['Casual', 'Formal']
 
     def __init__(self):
         self.product_name = self.generate_name()
         self.product_description = self.generate_description()
-        self.product_options = self.generate_options()
+        self.product_options = self.generate_duplicate_options()
 
     def fill_products(self):
         self.fake.add_provider(lorem)
@@ -56,7 +59,7 @@ class FakeProduct:
     def generate_status(self):
         return "draft"
 
-    def generate_options(self):
+    def generate_duplicate_options(self):
         return [
             {
                 "option_name": "color",
@@ -76,6 +79,79 @@ class FakeProduct:
             }
         ]
 
+    def generate_uniq_options(self):
+        return [
+            {
+                "option_name": "color",
+                "items": self.option_color_items[:2]
+            },
+            {
+                "option_name": "size",
+                "items": self.option_size_items[:2]
+            },
+            {
+                "option_name": "material",
+                "items": self.option_material_items[:2]
+            }
+        ]
+
+    def generate_random_options(self):
+
+        selected_options = random.sample(self.options, random.randint(0, 3))
+
+        # Select items based on the selected options
+        if len(selected_options) > 0:
+            selected_items = []
+            for option in selected_options:
+                match option:
+                    case 'color':
+                        option1 = {
+                            "option_name": option,
+                            "items": random.sample(self.option_color_items, random.randint(1, 5))
+
+                        }
+                        selected_items.append(option1)
+
+                    case 'size':
+                        option2 = {
+                            "option_name": option,
+                            "items": random.sample(self.option_size_items, random.randint(1, 5))
+
+                        }
+                        selected_items.append(option2)
+                    case 'material':
+                        option3 = {
+                            "option_name": option,
+                            "items": random.sample(self.option_material_items, random.randint(1, 5))
+
+                        }
+                        selected_items.append(option3)
+
+            return selected_items
+        else:
+            return []
+
+    def generate_uniq_options_more_than_tree(self):
+
+        return [
+            {
+                "option_name": "color",
+                "items": self.option_color_items[:2]
+            },
+            {
+                "option_name": "size",
+                "items": self.option_size_items[:2]
+            },
+            {
+                "option_name": "material",
+                "items": self.option_material_items[:2]
+            },
+            {
+                "option_name": "style",
+                "items": self.option_style_items[:2]
+            }
+        ]
+
     def populate_products(self):
         fake = Faker()
         fake.name()
@@ -85,6 +161,22 @@ class FakeProduct:
             description=fake.paragraph(nb_sentences=50),
             options=''
         )
+
+    def generate_duplicate_items_in_options(self):
+        return [
+            {
+                "option_name": "color",
+                "items": ['red', 'green', 'red', 'blue', 'red', 'blue', 'red']
+            },
+            {
+                "option_name": "size",
+                "items": ['S', 'M', 'M', 'XL', 'XL']
+            },
+            {
+                "option_name": "material",
+                "items": ['Cotton', 'Nylon']
+            }
+        ]
 
     def populate_simple_product(self):
         ...
