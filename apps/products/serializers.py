d0c7a86d249351a4f36cfb601dfc338d24972e70
AminZamani <aminzamani.work@gmail.com>
2023-08-18 12:29:52 +0330

    add, validate_options method to ProductCreateSerializer
    - validate options data before save them

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -25,6 +25,32 @@ class ProductCreateSerializer(serializers.ModelSerializer):
         model = Product
         fields = ['product_name', 'description', 'status', 'options']
 
+    def validate_options(self, options):
+        """
+        Merge dictionaries with the same "option_name" and make the "items" unique.
+        Remove options with an empty "items" list.
+        Check max 3 options per product.
+        """
+        merged_options = {}
+
+        for option in options:
+            option_name = option['option_name']
+            items = option['items']
+
+            if items:
+                if option_name in merged_options:
+                    merged_options[option_name].update(items)
+                else:
+                    merged_options[option_name] = set(items)
+
+        unique_options = [
+            {"option_name": option_name, "items": list(items)}
+            for option_name, items in merged_options.items()
+        ]
+        if len(unique_options) > 3:
+            raise serializers.ValidationError("A product can have a maximum of 3 options.")
+        return unique_options
+
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
